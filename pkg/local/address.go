1a1c4949d1a7a615390ff8f9fce1351710998af1
Roy <yordivad@gmail.com>
2020-10-20 21:52:31 -0600

    Fixing error on server

--- pkg/local/address.go
+++ pkg/local/address.go
@@ -1,71 +1,70 @@
 package local
 
 import (
-  "context"
-  "errors"
-  "github.com/gogo/protobuf/proto"
-  "github.com/mlambda-net/monads/monad"
-  "github.com/mlambda-net/net/pkg/common"
-  "github.com/mlambda-net/net/pkg/core"
-  "log"
-  "reflect"
-  "strings"
-  "time"
+	"context"
+	"errors"
+	"github.com/gogo/protobuf/proto"
+	"github.com/mlambda-net/monads/monad"
+	"github.com/mlambda-net/net/pkg/common"
+	"github.com/mlambda-net/net/pkg/core"
+	"log"
+	"reflect"
+	"strings"
+	"time"
 )
 
 type Address interface {
-  Future(proto.Message, time.Duration) monad.Future
-  Send(proto.Message)
+	Future(proto.Message, time.Duration) monad.Future
+	Send(proto.Message)
 }
 
 type address struct {
-  client     core.ConnectorClient
-  serializer common.Serializer
-  kind       string
+	client     core.ConnectorClient
+	serializer common.Serializer
+	kind       string
 }
 
+func (a address) Future(message proto.Message, timeout time.Duration) monad.Future {
 
-func (a address) Future(message proto.Message,timeout time.Duration) monad.Future {
+	f := monad.NewFuture()
+	go func() {
 
-  f := monad.NewFuture()
-  go func() {
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
 
-    ctx, cancel := context.WithTimeout(context.Background(), timeout)
-    defer cancel()
+		data, err := a.serializer.Serialize(message)
+		if err != nil {
+			f.SetResult(err)
+		}
 
-    data, err := a.serializer.Serialize(message)
-    if err != nil {
-      f.SetResult(err)
-    }
+		t := reflect.TypeOf(message)
+		res, err := a.client.Call(ctx, &core.Request{
+			Type:    strings.ReplaceAll(t.String(), "*", ""),
+			Payload: data,
+			Kind:    a.kind,
+		})
 
-    t := reflect.TypeOf(message)
-    res, err := a.client.Call(ctx, &core.Request{
-      Type:    strings.ReplaceAll(t.String(), "*", ""),
-      Payload: data,
-      Kind:    a.kind,
-    })
-
-    if err != nil {
-      f.SetResult(err)
-    }
-
-    if res.Status != 200 {
-      f.SetResult(errors.New(res.Message))
-    } else {
-      v, e := a.serializer.Deserialize(res.Message, res.Payload)
-      if e != nil {
-        f.SetResult(err)
-      } else {
-        f.SetResult(v)
-      }
-    }
-  }()
-  return f
+		if err != nil {
+			f.SetResult(err)
+		} else {
+			if res.Status != 200 {
+				f.SetResult(errors.New(res.Message))
+			} else {
+				v, e := a.serializer.Deserialize(res.Message, res.Payload)
+				if e != nil {
+					f.SetResult(err)
+				} else {
+					f.SetResult(v)
+				}
+			}
+		}
+	}()
+	return f
 }
 
 func (a address) Send(message proto.Message) {
-  _, e:=  a.Future(message, 5*time.Second).Result()
-  if e != nil {
-    log.Fatal(e)
-  }
+	_, e := a.Future(message, 5*time.Second).Result()
+	if e != nil {
+		log.Fatal(e)
+	}
 }
